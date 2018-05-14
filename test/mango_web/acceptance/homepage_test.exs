39b6bc99a2bcd5f8586292435c92da31dfc66433
Nicole Pellicena <nicole.pellicena@unrulygroup.com>
2018-05-14 20:39:40 +0100

    Remove comments.

--- test/mango_web/acceptance/homepage_test.exs
+++ test/mango_web/acceptance/homepage_test.exs
@@ -5,23 +5,12 @@ defmodule MangoWeb.HomepageTest do
   hound_session()
 
   test "presence of featured products" do
-    ### Given ###
-    # There are two products Apple and Tomato priced at 100
-    # and 50 respectively
-    # With apple being the only seasonal products
-
-    ### WHEN ###
-    # I navigate to the homepage
     navigate_to("/")
 
-    ### THEN ###
-    # I expect the page title to be "Seasonal products"
     page_title = find_element(:css, ".page-title")
     |> visible_text
     assert page_title == "Seasonal products"
 
-    ### AND ###
-    # I expect Apple to be in the product displayed
     product = find_element(:css, ".product")
     product_name = find_within_element(product, :css, ".product-name")
     |> visible_text
@@ -29,12 +18,8 @@ defmodule MangoWeb.HomepageTest do
     |> visible_text
 
     assert product_name == "Apple"
-    ### AND ###
-    # I expect it's price to be displayed on the screen
     assert product_price == "100"
 
-    ### AND ###
-    # I expect that Tomato is not present on the screen
     refute page_source() =~ "Tomato"
   end
 
