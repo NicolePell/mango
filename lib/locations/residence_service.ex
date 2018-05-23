83a2b1b85eb7f9ed939b06afa384161512bed61f
Nicole Pellicena <nicole.pellicena@unrulygroup.com>
2018-05-23 12:23:18 +0100

    Add navigation based on a user being signed in.

--- lib/locations/residence_service.ex
+++ lib/locations/residence_service.ex
@@ -1,5 +1,5 @@
 defmodule Locations.ResidenceService do
   def list_areas do
-    ["Earth", "Magrathea", "Frogstar World B"]
+    ["Earth", "Magrathea", "Frogstar World B", "Betelgeuse Five"]
   end
 end
