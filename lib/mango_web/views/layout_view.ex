e5cd74fec90663c0a823de6e6350f15d8d405d70
Nicole Pellicena <nicole.pellicena@unrulygroup.com>
2018-05-30 12:34:47 +0100

    Move complex eex expression to layoutview.

--- lib/mango_web/views/layout_view.ex
+++ lib/mango_web/views/layout_view.ex
@@ -3,4 +3,10 @@ defmodule MangoWeb.LayoutView do
 
   import MangoWeb.CartView, only: [cart_count: 1]
   
+  def cart_link_text(conn) do
+    raw """
+      <i class="fa fa-shopping-cart"></i> <span class="cart-count">#{cart_count(conn)}</span>
+    """
+  end
+
 end
